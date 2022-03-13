66d9e4bed374d29006c629e6db129fa96e618668
TinoKrueger <krueger.tino@googlemail.com>
2022-03-13 16:12:44 +0100

    Added text extraction extensions for PdfSharpCore.Pdf.PdfDocument

---
+++ Eshava.Report.Pdf.NetCore/Models/TextContent.cs
@@ -0,0 +1,10 @@
+﻿namespace Eshava.Report.Pdf.Models
+{
+	public class TextContent
+	{
+		public int PageIndex { get; set; }
+		public double PosX { get; set; }
+		public double PosY { get; set; }
+		public string Value { get; set; }
+	}
+}
